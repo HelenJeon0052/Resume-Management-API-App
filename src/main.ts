31427c9d03ecfd3b42d7c678afe9ef77600f8f17
HelenJeon0052 <jeon.isavelle@gmail.com>
2025-03-25 21:52:57 -0700

    minor

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  const port = process.env.PORT || 8000;
+  const configService = app.get(ConfigService)
+  const port = (config: ConfigService): number => {
+    return config.get('PORT') || 8000
+  }
 
   const config = new DocumentBuilder()
     .setTitle('NestJS Resume-Management')
@@ -24,7 +28,7 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(port);
-  console.log(`running on port ${port}`)
+  await app.listen(port(configService));
+  console.log(`running on port ${port(configService)}`)
 }
 bootstrap();
